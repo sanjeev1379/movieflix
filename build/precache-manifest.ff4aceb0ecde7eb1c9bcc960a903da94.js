789191120a4ddcb47c7927e22377df7940a514d4
Sanjeev Kumar <prsanjeev02@gmail.com>
2021-06-03 19:20:26 +0530

    build genrated

---
+++ build/precache-manifest.ff4aceb0ecde7eb1c9bcc960a903da94.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f4a96b0a4a03e72e528562c793ee01",
+    "url": "/darwin619/Movieflix-Clone/index.html"
+  },
+  {
+    "revision": "4d5582fc4902877a42a9",
+    "url": "/darwin619/Movieflix-Clone/static/css/1.9d9a5925.chunk.css"
+  },
+  {
+    "revision": "340c0ea6a56b85ebf739",
+    "url": "/darwin619/Movieflix-Clone/static/css/5.08b40f2a.chunk.css"
+  },
+  {
+    "revision": "c42f298a9b212b07e298",
+    "url": "/darwin619/Movieflix-Clone/static/css/6.08b40f2a.chunk.css"
+  },
+  {
+    "revision": "f8d79bf3b4f38af67e8e",
+    "url": "/darwin619/Movieflix-Clone/static/css/7.3c9a809d.chunk.css"
+  },
+  {
+    "revision": "bc1bdcf350be9373e031",
+    "url": "/darwin619/Movieflix-Clone/static/css/8.3c9a809d.chunk.css"
+  },
+  {
+    "revision": "e31b6c59e3c8edd716e5",
+    "url": "/darwin619/Movieflix-Clone/static/css/main.e1605052.chunk.css"
+  },
+  {
+    "revision": "520879241a755ba38fcf",
+    "url": "/darwin619/Movieflix-Clone/static/js/0.7b74a6dc.chunk.js"
+  },
+  {
+    "revision": "4d5582fc4902877a42a9",
+    "url": "/darwin619/Movieflix-Clone/static/js/1.6e5bc1b6.chunk.js"
+  },
+  {
+    "revision": "8b75d4ac80dcf02d22da",
+    "url": "/darwin619/Movieflix-Clone/static/js/4.2f1d1f81.chunk.js"
+  },
+  {
+    "revision": "340c0ea6a56b85ebf739",
+    "url": "/darwin619/Movieflix-Clone/static/js/5.9b4f0984.chunk.js"
+  },
+  {
+    "revision": "c42f298a9b212b07e298",
+    "url": "/darwin619/Movieflix-Clone/static/js/6.19a09466.chunk.js"
+  },
+  {
+    "revision": "f8d79bf3b4f38af67e8e",
+    "url": "/darwin619/Movieflix-Clone/static/js/7.4e25b110.chunk.js"
+  },
+  {
+    "revision": "bc1bdcf350be9373e031",
+    "url": "/darwin619/Movieflix-Clone/static/js/8.1539e181.chunk.js"
+  },
+  {
+    "revision": "e31b6c59e3c8edd716e5",
+    "url": "/darwin619/Movieflix-Clone/static/js/main.43666251.chunk.js"
+  },
+  {
+    "revision": "74682783de766c85de3f",
+    "url": "/darwin619/Movieflix-Clone/static/js/runtime~main.41b41669.js"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/darwin619/Movieflix-Clone/static/media/Background.d2edb29f.jpg"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/darwin619/Movieflix-Clone/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "d902c77253bec61f403a5978d2e7c858",
+    "url": "/darwin619/Movieflix-Clone/static/media/netflix.d902c772.jpg"
+  },
+  {
+    "revision": "9179862c82e20d655c43c9b468b96aa4",
+    "url": "/darwin619/Movieflix-Clone/static/media/star.9179862c.png"
+  }
+]);
