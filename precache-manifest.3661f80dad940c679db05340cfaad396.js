5c01f256e7fc165fa3f98940a2d1d800e2a18d7a
Sanjeev Kumar <prsanjeev02@gmail.com>
2021-06-03 20:14:45 +0530

    Updates

---
+++ precache-manifest.3661f80dad940c679db05340cfaad396.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59669103be930ddfa7a1d75c6de21078",
+    "url": "/movieflix/index.html"
+  },
+  {
+    "revision": "4d5582fc4902877a42a9",
+    "url": "/movieflix/static/css/1.9d9a5925.chunk.css"
+  },
+  {
+    "revision": "340c0ea6a56b85ebf739",
+    "url": "/movieflix/static/css/5.08b40f2a.chunk.css"
+  },
+  {
+    "revision": "c42f298a9b212b07e298",
+    "url": "/movieflix/static/css/6.08b40f2a.chunk.css"
+  },
+  {
+    "revision": "f8d79bf3b4f38af67e8e",
+    "url": "/movieflix/static/css/7.3c9a809d.chunk.css"
+  },
+  {
+    "revision": "bc1bdcf350be9373e031",
+    "url": "/movieflix/static/css/8.3c9a809d.chunk.css"
+  },
+  {
+    "revision": "dc666a734f4795788f95",
+    "url": "/movieflix/static/css/main.e1605052.chunk.css"
+  },
+  {
+    "revision": "520879241a755ba38fcf",
+    "url": "/movieflix/static/js/0.7b74a6dc.chunk.js"
+  },
+  {
+    "revision": "4d5582fc4902877a42a9",
+    "url": "/movieflix/static/js/1.6e5bc1b6.chunk.js"
+  },
+  {
+    "revision": "425ade9569d991def4c8",
+    "url": "/movieflix/static/js/4.469736e2.chunk.js"
+  },
+  {
+    "revision": "340c0ea6a56b85ebf739",
+    "url": "/movieflix/static/js/5.9b4f0984.chunk.js"
+  },
+  {
+    "revision": "c42f298a9b212b07e298",
+    "url": "/movieflix/static/js/6.19a09466.chunk.js"
+  },
+  {
+    "revision": "f8d79bf3b4f38af67e8e",
+    "url": "/movieflix/static/js/7.4e25b110.chunk.js"
+  },
+  {
+    "revision": "bc1bdcf350be9373e031",
+    "url": "/movieflix/static/js/8.1539e181.chunk.js"
+  },
+  {
+    "revision": "dc666a734f4795788f95",
+    "url": "/movieflix/static/js/main.48575427.chunk.js"
+  },
+  {
+    "revision": "3b29c5575d79e406b87d",
+    "url": "/movieflix/static/js/runtime~main.e9987611.js"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/movieflix/static/media/Background.d2edb29f.jpg"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/movieflix/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "d902c77253bec61f403a5978d2e7c858",
+    "url": "/movieflix/static/media/netflix.d902c772.jpg"
+  },
+  {
+    "revision": "9179862c82e20d655c43c9b468b96aa4",
+    "url": "/movieflix/static/media/star.9179862c.png"
+  }
+]);
